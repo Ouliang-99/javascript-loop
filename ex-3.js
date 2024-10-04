cbf178d19468989109b956b7407e1634d103f684
podusit <d.rimdusit@gmail.com>
2024-10-04 14:08:33 +0700

    hackhour:JavaScript Loop Assignment

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,14 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore;
+// กำหนดค่าต่ำสุดเริ่มต้นเป็นค่าแรกใน array
+let minScore = scores[0]
 // Start coding here
+// ใช้ loop for เพื่อตรวจสอบแต่ละค่าใน array
+for (let i=0; i < scores.length;i++) {
+    if (scores[i] < minScore) {
+         // หากพบค่าที่น้อยกว่าค่าต่ำสุดที่เก็บไว้ ให้ปรับค่า
+        minScore = scores[i]
+    }
+}
 
 console.log(minScore);
