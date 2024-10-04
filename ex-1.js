a6c0b5c64ee42b0d95c49dcf4408b62fa88464a7
OLEANG <wannachok.job@gmail.com>
2024-10-04 13:47:36 +0700

    oleang:answer-for-javascript-loop

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,11 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
+let newScore = [];
 
 // Start coding here
+
+for (let i = 0; i < scores.length; i++) {
+    newScore.push(scores[i] - 10 ) ;
+};
+
+console.log(newScore);
